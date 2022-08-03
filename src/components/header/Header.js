c274c8536a86d02b1bc57b5a5578b0b0ccfc51d7
Philip <philipmjobe@gmail.com>
2022-08-03 11:21:01 -0400

    starts building nav bar with login logout capacity and icons

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -5,3 +5,34 @@ import SearchIcon from "@material-ui/icons/Search";
 import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket";
 import { useStateValue } from "../stateProvider/StateProvider";
 import { auth } from "firebase";
+function Header() {
+  const [{ basket, user }] = useStateValue();
+  const history = useHistory();
+  const login = () => {
+    if (user) {
+      auth().signout();
+      history.push("/login");
+    }
+  };
+  return (
+    <nav className="header">
+      {/* logo on the left -> img */}
+      <Link to="/">
+        {/* search box */}
+        <div className="header__search">
+          <input type="text" className="header__searchInput" />
+          <SearchIcon className="header__SearchIcon" />
+        </div>
+        {/* 3 links */}
+        <div clssName="header__nav">
+          <Link to={!user && "/login"} className="header__link">
+            <div onClick={login} className="header__option">
+              <span className="header__optionLineOne">Hello {user?.email}</span>
+              <span className="header__optionLineTwo">{user ? "Sign Out" : "Sign In"}</span>
+            </div>
+          </Link>
+        </div>
+      </Link>
+    </nav>
+  );
+}
