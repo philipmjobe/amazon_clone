cb0f24341a6d7137612bff36e6f551888540566f
Philip <philipmjobe@gmail.com>
2022-08-04 14:17:55 -0400

    adds Subtotal.js and imports react and currenct format

---
+++ src/components/subtotal/Subtotal.js
@@ -0,0 +1,5 @@
+import React from "react";
+import CurrencyFormat from "react-currency-format";
+import { useStateValue } from "../stateProvider/StatProvider";
+import "./Subtotal.css";
+import { getBasketTotal } from "../../reducer";
