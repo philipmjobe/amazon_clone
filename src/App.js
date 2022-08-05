17c4e7d78bcd8578711a8735dcfc380364a5b1ac
Philip <philipmjobe@gmail.com>
2022-08-05 10:21:39 -0400

    refactors App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "/App";
+import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import { StateProvider } from "../src/components/stateProvider/StateProvider";
 import reducer, { initialState } from "./reducer";
-
 ReactDOM.render(
   <React.StrictMode>
     <StateProvider initialState={initialState} reducer={reducer}>
@@ -14,20 +13,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById("root")
 );
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-export default App;
